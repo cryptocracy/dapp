56f514349a2e435b907fab1d6ad1652efed3997f
Akash Kaushik <akash.kaushik@indiqus.com>
2018-09-01 12:05:55 +0530

    server port changes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors')
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8081;
 const history = require('connect-history-api-fallback');
 
 // 
