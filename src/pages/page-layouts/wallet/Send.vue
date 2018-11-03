08054e1ff1820c8ccfb5fb81f1a5170e784ec834
Alexey <alexii.nnn@gmail.com>
2018-11-03 04:05:52 +0100

    tx send fix + loader

--- src/pages/page-layouts/wallet/Send.vue
+++ src/pages/page-layouts/wallet/Send.vue
@@ -1,29 +1,30 @@
 <template>
-  <v-layout row>
+  <v-layout row v-if="!stateIsLoading">
     <v-flex xs12 sm8 offset-sm2>
+      <v-card class="wallet-card"></v-card>
       <v-card class="wallet-card">
         <div class="wallet-title">Send to:</div>
         <v-form ref="form" v-model="valid" lazy-validation>
           <v-text-field
-            v-model="addressee"
-            :rules="addressRules"
-            :disabled="isLoading"
-            label="Addressee"
-            required
+              v-model="addressee"
+              :rules="addressRules"
+              :disabled="isLoading"
+              label="Addressee"
+              required
           ></v-text-field>
           <v-text-field
-            v-model="amountPay"
-            label="Amount to pay"
-            :disabled="isLoading"
-            :rules="amountRule"
-            suffix="BTC (in Satoshi)"
+              v-model="amountPay"
+              label="Amount to pay"
+              :disabled="isLoading"
+              :rules="amountRule"
+              suffix="BTC (in Satoshi)"
           ></v-text-field>
           <v-text-field
-            v-model="amountFee"
-            label="Transaction fee"
-            :disabled="isLoading"
-            :rules="amountRule"
-            suffix="Satoshi/byte"
+              v-model="amountFee"
+              label="Transaction fee"
+              :disabled="isLoading"
+              :rules="amountRule"
+              suffix="Satoshi/byte"
           ></v-text-field>
           <div class="fee-title" v-if="fastestFee || halfHourFee || hourFee">Recommended fees (in Satoshis per byte)</div>
           <div class="fee-hints">
@@ -39,14 +40,14 @@
           </div>
           <div class="form-buttons">
             <v-btn
-              :disabled="!valid || isLoading"
-              @click="submit"
+                :disabled="!valid || isLoading"
+                @click="submit"
             >
               send
             </v-btn>
             <v-btn
-              @click="clear"
-              :disabled="isLoading"
+                @click="clear"
+                :disabled="isLoading"
             >
               clear
             </v-btn>
@@ -58,6 +59,7 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 const bitcoin = require('bitcoinjs-lib')
 const CoinKey = require('coinkey')
 const axios = require('axios')
@@ -85,6 +87,11 @@ export default {
     ],
     valid: false
   }),
+  computed: {
+    ...mapGetters({
+      stateIsLoading: 'isLoading'
+    })
+  },
   methods: {
     setFee (amount) {
       this.amountFee = amount
@@ -97,19 +104,29 @@ export default {
     submit () {
       this.isLoading = true
       let tx = new bitcoin.TransactionBuilder()
-      for (let txData in this.addressData.txs) {
-        for (let out in txData.out) {
+      for (let txData of this.addressData.txs) {
+        console.log('------')
+        console.log(txData)
+        console.log('------')
+
+        for (let out of txData.out) {
+          console.log(out)
           if (!out.spent && out.addr === this.addressPublic) {
             tx.addInput(txData.hash, +out.n)
+            console.log('new input')
           }
         }
       }
       tx.addOutput(this.addressee, +this.amountPay)
       tx.addOutput(this.addressPublic, this.addressData.final_balance - this.amountPay - this.amountFee * (tx.__tx.ins.length * 148 + (tx.__tx.outs.length + 1) * 34 + 10 - tx.__tx.ins.length))
+      console.log(tx)
       const buffer = Buffer.from(JSON.parse(localStorage['blockstack']).appPrivateKey, 'hex')
       const ck = new CoinKey(buffer) // eslint-disable-next-line
       let keyPair = new bitcoin.ECPair.fromWIF(ck.privateWif, bitcoin.networks.bitcoin)
-      tx.sign(0, keyPair)
+      for (let inputIndex in tx.__inputs) {
+        console.log(inputIndex)
+        tx.sign(+inputIndex, keyPair)
+      }
       let txHex = tx.build().toHex()
       let bodyFormData = new FormData()
       bodyFormData.append('tx', txHex)
@@ -127,20 +144,23 @@ export default {
     }
   },
   mounted () {
+    this.$store.commit('toggleLoading')
     this.addressPublic = JSON.parse(localStorage['blockstack-gaia-hub-config']).address
-    axios.get('https://bitcoinfees.earn.com/api/v1/fees/recommended')
-      .then((res) => {
-        this.fastestFee = res.data.fastestFee
-        this.halfHourFee = res.data.halfHourFee
-        this.hourFee = res.data.hourFee
-      })
-
+    //  get info about wallet address
     axios.get(apiUrl + this.addressPublic)
       .then((res) => {
         console.log('------')
         console.log(res)
         console.log('------')
         this.addressData = res.data
+        this.$store.commit('toggleLoading')
+      })
+    //  get info about tx fees
+    axios.get('https://bitcoinfees.earn.com/api/v1/fees/recommended')
+      .then((res) => {
+        this.fastestFee = res.data.fastestFee
+        this.halfHourFee = res.data.halfHourFee
+        this.hourFee = res.data.hourFee
       })
   }
 }
