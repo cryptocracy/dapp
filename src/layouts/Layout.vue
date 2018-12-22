66bd8d577cbef2dd9fbf72ae527d272ec6f2db75
Akash Kaushik <akash.kaushik@indiqus.com>
2018-12-23 00:59:02 +0530

    no data message added

--- src/layouts/Layout.vue
+++ src/layouts/Layout.vue
@@ -5,11 +5,12 @@
     </div>
     <div v-if="showGraph" class="panel-body" :is="children" :style="{height: height}">
     </div>
-    <div v-else class="panel-body text-xs-center"  :style="{height: height}">
+    <div v-else class="panel-body text-xs-center no-data"  :style="{height: height}">
       <v-icon>fas fa-exchange-alt</v-icon>
-      <h4 class="mt-5">
+      <h4 class="mt-5 no-data">
         No transactions found.
       </h4>
+      <p>Looks like you have not done any transactions yet.</p>
     </div>
   </div>
 </template>
@@ -27,4 +28,7 @@ export default {
 </script>
 
 <style lang="css">
+  .no-data {
+    color: rgba(0,0,0,.54)
+  }
 </style>
