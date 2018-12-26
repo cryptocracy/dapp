b4ecdb33eea3cc7ba8af8a79a917bcf33affdfff
Cryptocracy <cryptocracy@users.noreply.github.com>
2018-12-26 11:19:15 -0800

    http to https
    http to https

--- src/services/cryptocracy.js
+++ src/services/cryptocracy.js
@@ -2,7 +2,7 @@ import Axios from 'axios'
 
 const cryptocracyServices = {
   proximitySearch: (query) => {
-    return Axios.get('http://core.cryptocracy.io/souq/search/proximity', { params: {...query} })
+    return Axios.get('https://core.cryptocracy.io/souq/search/proximity', { params: {...query} })
   }
 }
 export default cryptocracyServices
