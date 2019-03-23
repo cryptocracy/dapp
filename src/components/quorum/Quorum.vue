626f9b8248ad925b3980f7b3938d153e7ee78b8c
akashkaushik33 <akashkaushik33@gmail.com>
2019-03-24 01:55:48 +0530

    quorum page UI improvments.

--- src/components/quorum/Quorum.vue
+++ src/components/quorum/Quorum.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container>
     <v-layout row wrap justify-center>
-      <v-flex xs10>
+      <v-flex xs12>
         <v-layout justify-end row wrap>
           <v-flex xs3 class='quorumList'>
             <v-select
@@ -14,38 +14,86 @@
           </v-flex>
         </v-layout>
 
-        <v-flex class="quorumtour">
-          <v-expansion-panel :hover="true" class="titlePosition br20 mt-2" v-for="(item, index) in quorumData" :key="index">
-            <span class='quorumTitle' v-if="lazyLoadedData[item.contentUrl]">{{lazyLoadedData[item.contentUrl].title}}</span>
-            <img v-if="lazyLoadedData[item.contentUrl] && lazyLoadedData[item.contentUrl].image" width='759' height='300' :src="lazyLoadedData[item.contentUrl].image" alt="avatar">
-            <v-expansion-panel-content>
-              <div slot="header" v-if="lazyLoadedData[item.contentUrl]" class="fn16 displayFlex justify-space-between">
-                <span><v-icon color='blue'>thumbs_up_down</v-icon></span>
+        <v-layout class="quorumtour" row wrap>
+          <v-flex :class="index === 0 ? 'xs12' : 'xs6'" v-for="(item, index) in quorumData" :key="index" >
+            <!-- <v-expansion-panel :hover="true" class="titlePosition br20 mt-2" v-for="(item, index) in quorumData" :key="index">
+              <span class='quorumTitle' v-if="lazyLoadedData[item.contentUrl]">{{lazyLoadedData[item.contentUrl].title}}</span>
+              <img height="200px" v-if="lazyLoadedData[item.contentUrl] && lazyLoadedData[item.contentUrl].image"  :src="lazyLoadedData[item.contentUrl].image" alt="avatar">
+              <v-expansion-panel-content>
+                <div slot="header" v-if="lazyLoadedData[item.contentUrl]" class="fn16 displayFlex justify-space-between">
+                  <span><v-icon color='blue'>thumbs_up_down</v-icon></span>
+                  <span class='iconGroup'>
+                    <v-icon class='iconStyle'>edit</v-icon>
+                    <v-icon class='iconStyle' color="red ">place</v-icon>
+                    <v-icon class='iconStyle' color="green ">image</v-icon>
+                    <v-icon class='iconStyle' color="yellow ">today</v-icon>
+                    <v-icon class='iconStyle' color="brown ">account_balance_wallet</v-icon>
+                    <v-icon class='iconStyle' color="red ">favorite</v-icon>
+                    <v-icon class='iconStyle' color="green ">share</v-icon>
+                  </span>
+                </div>
+                <v-card>
+                  <v-card-text class="py12px24">
+                    <v-flex v-if="lazyLoadedData[item.contentUrl]" class="space-between">
+                      <div>{{type(item.contentUrl)}}</div>
+                      <v-btn dark color="teal accent-4" @click="redirectUser(item)" ><v-icon dark>keyboard_arrow_right</v-icon></v-btn>
+                    </v-flex>
+                    <div class="v-list__tile__sub-title" v-if="lazyLoadedData[item.contentUrl]" >{{lazyLoadedData[item.contentUrl].description}}</div>
+                    <div class="v-list__tile__sub-title"><strong>Votes: {{item.votes}}</strong></div>
+                    <div class="v-list__tile__sub-title"><strong>Content URL:</strong> <span>{{item.contentUrl}}</span></div>
+                    <div class="v-list__tile__sub-title"><strong>Created On:</strong> {{new Date(Number(item.createdOn)).toDateString()}}</div>
+                  </v-card-text>
+                </v-card>
+              </v-expansion-panel-content>
+            </v-expansion-panel> -->
+
+            <v-card hover class=" mt-2 br20" >
+              <v-img
+                :src="lazyLoadedData[item.contentUrl] ? lazyLoadedData[item.contentUrl].image : ''"
+                height="250px"
+                alt="No image"
+              >
+              </v-img>
+
+              <v-card-title v-if="lazyLoadedData[item.contentUrl]" primary-title>
+                <div>
+                  <div class="headline">{{lazyLoadedData[item.contentUrl].title}}</div>
+                  <span class="grey--text">{{lazyLoadedData[item.contentUrl].description || lazyLoadedData[item.contentUrl].detail}}</span>
+                </div>
+              </v-card-title>
+
+              <v-card-actions>
+                <v-icon color='blue'>thumbs_up_down</v-icon> <span class="subheading ml-2">{{item.votes}}</span>
+                <v-spacer></v-spacer>
                 <span class='iconGroup'>
-                  <v-icon class='iconStyle'>edit</v-icon>
-                  <v-icon class='iconStyle' color="red ">place</v-icon>
+                  <!-- <v-icon class='iconStyle'>edit</v-icon> -->
+                  <!-- <v-icon class='iconStyle' color="red ">place</v-icon>
                   <v-icon class='iconStyle' color="green ">image</v-icon>
-                  <v-icon class='iconStyle' color="yellow ">today</v-icon>
+                  <v-icon class='iconStyle' color="yellow ">today</v-icon> -->
                   <v-icon class='iconStyle' color="brown ">account_balance_wallet</v-icon>
                   <v-icon class='iconStyle' color="red ">favorite</v-icon>
-                  <v-icon class='iconStyle' color="green ">share</v-icon>
+                  <!-- <v-icon class='iconStyle' color="green ">share</v-icon> -->
                 </span>
-              </div>
-              <v-card>
-                <v-card-text class="py12px24">
-                  <v-flex v-if="lazyLoadedData[item.contentUrl]" class="space-between">
-                    <div>{{type(item.contentUrl)}}</div>
-                    <v-btn dark color="teal accent-4" @click="redirectUser(item)" ><v-icon dark>keyboard_arrow_right</v-icon></v-btn>
-                  </v-flex>
+                <v-btn icon @click="func(index)">
+                  <v-icon>{{ showIndex[index] ? 'keyboard_arrow_down' : 'keyboard_arrow_up' }}</v-icon>
+                </v-btn>
+              </v-card-actions>
+
+              <v-slide-y-transition>
+                <v-card-text v-if="showIndex[index]">
+
+                  <v-chip color="primary" class="white--text" v-for="(tag, index1) in lazyLoadedData[item.contentUrl].tags || []" :key="index1" >
+                    {{'#' + tag.title}}
+                  </v-chip>
                   <!-- <div class="v-list__tile__sub-title" v-if="lazyLoadedData[item.contentUrl]" >{{lazyLoadedData[item.contentUrl].description}}</div> -->
-                  <div class="v-list__tile__sub-title"><strong>Votes: {{item.votes}}</strong></div>
+                  <!-- <div class="v-list__tile__sub-title"><strong>Votes: {{item.votes}}</strong></div> -->
                   <div class="v-list__tile__sub-title"><strong>Content URL:</strong> <span>{{item.contentUrl}}</span></div>
                   <div class="v-list__tile__sub-title"><strong>Created On:</strong> {{new Date(Number(item.createdOn)).toDateString()}}</div>
                 </v-card-text>
-              </v-card>
-            </v-expansion-panel-content>
-          </v-expansion-panel>
-        </v-flex>
+              </v-slide-y-transition>
+            </v-card>
+          </v-flex>
+        </v-layout>
       </v-flex>
     </v-layout>
   </v-container>
@@ -60,13 +108,27 @@ export default {
       { text: 'Votes Sum', value: 'sum' }
     ],
     sortBy: 'pop',
-    lazyLoadedData: {}
+    lazyLoadedData: {},
+    show: false,
+    showIndex: {}
   }),
   computed: {
     ...mapGetters({
       quorumData: 'getQuorumData',
       contentUrlsData: 'getcontentUrlsData'
     })
+    // showIndex () {
+    //   console.log('HEREEEEE')
+    //   if (this.quorumData && this.quorumData.length > 0) {
+    //     let a = {}
+    //     this.quorumData.forEach((item, index) => {
+    //       a[index] = false
+    //     })
+    //     console.log(a)
+    //     return a
+    //   }
+    //   return {}
+    // }
   },
   watch: {
     deep: true,
@@ -78,6 +140,11 @@ export default {
     }
   },
   methods: {
+    func (index) {
+      console.log('INDEXXX', index)
+      this.showIndex[index] = !this.showIndex[index]
+      console.log(this.showIndex)
+    },
     type (contentUrl) {
       let type = contentUrl.includes('marker') ? 'MARKER'
         : contentUrl.includes('image') ? 'IMAGE'
@@ -100,7 +167,18 @@ export default {
     }
   },
   created () {
-    this.$store.dispatch('ACTION_GET_QUORUM_DATA', { sort: 'pop' })
+    if (this.quorumData && this.quorumData.length > 0) {
+      this.quorumData.forEach((item, index) => {
+        this.$set(this.showIndex, index, false)
+        // this.showIndex[index] = false
+      })
+    }
+    this.$store.dispatch('ACTION_GET_QUORUM_DATA', { sort: 'pop' }).then(res => {
+      for (let i = 0; i < res / 2; i++) {
+        // this.showIndex[i] = false
+        this.$set(this.showIndex, i, false)
+      }
+    })
   }
 }
 </script>
